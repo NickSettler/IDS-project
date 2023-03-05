27be47c10324667030a5fd0baee45d0c36c5c9c3
NickSettler <moiseevnikita14@gmail.com>
2023-03-05 14:12:19 +0100

    feat: add suite SQL script
    * drop all previous tables, views and procedures
    * create suite table
    * create suite_view
    * create insert_suite procedure
    * add inserts for testings

---
+++ scripts/suite.sql
@@ -0,0 +1,39 @@
+DROP TABLE XMOISE01.suite;
+DROP VIEW XMOISE01.suite_view;
+DROP PROCEDURE XMOISE01.insert_suite;
+
+CREATE TABLE XMOISE01.suite
+(
+    suite_number  INT NOT NULL PRIMARY KEY,
+    suite_type_id INT,
+    CONSTRAINT suite_type_id_fk FOREIGN KEY (suite_type_id) REFERENCES suite_type (id),
+    CONSTRAINT suite_number_fk CHECK ( suite_number > 1000 AND suite_number < 9999 )
+);
+
+CREATE VIEW XMOISE01.suite_view AS
+SELECT suite_number,
+       SUBSTR(suite_number, 1, 1) AS floor,
+       ST.CAPACITY,
+       ST.NAME,
+       ST.PRICE,
+       STS.ROOMS_COUNT,
+       STS.BEDS_COUNT,
+       STS.SUITE_VARIANT
+FROM XMOISE01.suite
+         JOIN SUITE_TYPE ST on ST.ID = SUITE.SUITE_TYPE_ID
+         JOIN SUITE_TYPE_SPEC STS on ST.ID = STS.SUITE_TYPE_ID;
+
+CREATE PROCEDURE XMOISE01.insert_suite(insert_number IN INT, insert_type_id IN INT)
+AS
+BEGIN
+    INSERT INTO XMOISE01.suite (suite_number, suite_type_id) VALUES (insert_number, insert_type_id);
+END;
+
+-- INSERTS FOR TESTING
+BEGIN
+    FOR suite_type IN (SELECT * FROM SUITE_TYPE) LOOP
+        insert_suite(suite_type.ID * 1000 + 1, suite_type.ID);
+        insert_suite(suite_type.ID * 1000 + 2, suite_type.ID);
+        insert_suite(suite_type.ID * 1000 + 3, suite_type.ID);
+    END LOOP;
+END;
