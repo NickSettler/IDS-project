9e3fa5ded1b3a9768463d84fee3b37b1fa8fed79
yulia3112 <94862503+yulia3112@users.noreply.github.com>
2023-03-26 16:41:37 +0200

    Update service_request.sql

--- scripts/service_request.sql
+++ scripts/service_request.sql
@@ -15,7 +15,12 @@ SELECT service_request.service_request_id,
 FROM service_request
     JOIN service s ON service_request.service_id = s.service_id;
 
-CREATE PROCEDURE insert_service_request (insert_request_id IN INT, insert_request_date IN DATE)
+CREATE PROCEDURE insert_service_request (
+    insert_request_id IN INT,
+    insert_request_date IN DATE,
+    insert_service_id IN INT,
+    insert_reservation_id IN INT
+    )
 AS
 BEGIN
   INSERT INTO service_request (service_request_id, service_request_date)
@@ -25,7 +30,7 @@ END;
 -- TESTING DATA
 
 BEGIN
-    insert_service_request (1, '01-04-2023');
-    insert_service_request (2, '02-04-2023');
-    insert_service_request (3, '03-04-2023');
+    insert_service_request (1, '01-04-2023', 1, 1);
+    insert_service_request (2, '02-04-2023', 2, 2);
+    insert_service_request (3, '03-04-2023', 3, 3);
 END;
